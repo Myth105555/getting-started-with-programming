61ea410aeca70dfe5414beee6596a50f08d3ba9c
Myth105555 <Mohita.anand0903@gmail.com>
2025-06-05 12:39:41 +0530

    first

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("hello")
+print("welcome to codingal \n")
+print("this is our 2nd class")
+print(7)
+V=8
+k=9
+print(V)
+print(k)
+name=input("enter your name   ")
+print("your name is", name )
